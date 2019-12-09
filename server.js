a15fb9cf276bb03aa0aaf6b16829251d714b2b85
Jamie Michel <Jamie.Michel@hotmail.com>
2019-12-09 16:17:28 +0000

    Users model created, ready to get working on the users API

--- server.js
+++ server.js
@@ -1,15 +1,23 @@
 const express = require('express');
 const connectDB = require('./config/db');
+
 //Instantiate app
 const app = express();
 //Connect to DB
 connectDB();
+
 //If no env set, will default to 5000
 const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
   res.send('API Running');
 });
+
+//Defining routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
 
 app.listen(PORT, () => {
   console.log(`Server started on Port: ${PORT}`);
